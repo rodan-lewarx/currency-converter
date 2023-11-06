166a5420fecd520d1aefd362e982f07483672fde
Antonín Simerský <antonin.simersky@live.com>
2023-11-06 09:55:44 +0100

    Add shared types and schema parsing to FE

--- apps/frontend/src/components/CurrencyConverter.tsx
+++ apps/frontend/src/components/CurrencyConverter.tsx
@@ -1,16 +1,23 @@
 import { useQuery } from "react-query";
+import { CurrencyListSchema } from "@task/currency-list-schema";
 
 const ENDPOINT_URL = import.meta.env.VITE_ENDPOINT_URL;
 
 const CurrencyConverter = () => {
   const { data: currencyList } = useQuery("currencyList", async () => {
     const response = await fetch(ENDPOINT_URL);
-    const data = await response.json();
 
-    if (data.length === 0) {
-      throw new Error("No data found");
+    if (!response.ok) {
+      throw new Error("Failed to fetch data");
+    }
+
+    const data = await response.json();
+    const parsedData = CurrencyListSchema.parse(data);
+
+    if (parsedData.length === 0) {
+      throw new Error("Failed to fetch data");
     } else {
-      return data;
+      return parsedData;
     }
   });
 
