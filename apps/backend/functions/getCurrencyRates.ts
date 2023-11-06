7deb4554b2a3155e81a7edc6e784d478073dac39
Antonín Simerský <antonin.simersky@live.com>
2023-11-06 09:24:22 +0100

    Reformat function

--- apps/backend/functions/getCurrencyRates.ts
+++ apps/backend/functions/getCurrencyRates.ts
@@ -2,11 +2,7 @@ import { Handler, HandlerEvent, HandlerContext } from "@netlify/functions";
 import { fetchAndParseData } from "../currencyParser/effects";
 import { Effect } from "effect";
 
-const handler: Handler = async (
-  event: HandlerEvent,
-  context: HandlerContext
-) => {
-  return await Effect.runPromise(fetchAndParseData);
-};
+const handler: Handler = async (event: HandlerEvent, context: HandlerContext) =>
+  await Effect.runPromise(fetchAndParseData);
 
 export { handler };
